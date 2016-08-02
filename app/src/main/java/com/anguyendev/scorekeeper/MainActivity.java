a113f828a14e0a29a9b5e5a911b17ed32f83bb20
Amanda Nguyen <amanda@Amandas-MacBook-Pro.local>
2016-08-01 23:27:26 -0500

    Replace list view with recycler view and resotre state on orientation change

--- app/src/main/java/com/anguyendev/scorekeeper/MainActivity.java
+++ app/src/main/java/com/anguyendev/scorekeeper/MainActivity.java
@@ -2,42 +2,64 @@ package com.anguyendev.scorekeeper;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 
-import com.anguyendev.scorekeeper.adapters.PlayerListViewAdapter;
+import com.anguyendev.scorekeeper.adapters.PlayerRecyclerViewAdapter;
 import com.anguyendev.scorekeeper.model.Player;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView mListView;
-    private List<Player> mPlayerList = new ArrayList<>();
+    private static final String STATE_PLAYER_LIST = "playerList";
+    private RecyclerView mRecyclerView;
+    private PlayerRecyclerViewAdapter mRecyclerAdapter;
+    private ArrayList<Player> mPlayerList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //TODO: Is this actually where I should be restoring this?
+        if (savedInstanceState != null) {
+            mPlayerList = (ArrayList) savedInstanceState.getSerializable(STATE_PLAYER_LIST);
+        }
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mListView = (ListView)findViewById(R.id.listview);
-        mListView.setAdapter(new PlayerListViewAdapter(getApplicationContext(), mPlayerList));
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        mRecyclerAdapter = new PlayerRecyclerViewAdapter(getApplicationContext(), mPlayerList);
+        mRecyclerView.setAdapter(mRecyclerAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //TODO remove stupid hardcode
                 mPlayerList.add(new Player("Player 1", 99));
+                mRecyclerAdapter.notifyItemInserted(mPlayerList.size()-1);
             }
         });
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mPlayerList = (ArrayList) savedInstanceState.getSerializable(STATE_PLAYER_LIST);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putSerializable(STATE_PLAYER_LIST, mPlayerList);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
